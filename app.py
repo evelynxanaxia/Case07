c38126c243333bf1ab63031611c5b9ce54c97783
Daughter-of-most-high53 <evelynxanixa.heartc@gmail.com>
2025-10-27 00:25:42 -0400

    Initial commit of azure files

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, jsonify, request, render_template
+from dotenv import load_dotenv
+import os
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+
+
+load_dotenv()  # reads .env into process env
+
+
+app = Flask(__name__)
+blob_service_client = BlobServiceClient.from_connection_string(os.environ.get('AZURE_STORAGE_CONNECTION_STRING'))
+CONTAINER_NAME = "images-demo"
+
+
+@app.route('/api/v1/health')
+def health():
+   return jsonify({
+       'status': 'ok'
+   }),200
+
+
+@app.route('/api/v1/upload', methods=['POST'])
+def upload():
+   file = request.files['file']
+   try:
+       container_client = blob_service_client.get_container_client(CONTAINER_NAME)
+       blob_client = container_client.get_blob_client(file.filename)
+       blob_client.upload_blob(file, overwrite=True)
+       return jsonify({
+       'ok': True,
+       'url': container_client.url + '/' + file.filename
+       }), 200
+   except Exception as e:
+       return jsonify({
+           'ok': False,
+           'error': str(e)  
+       }),500
+
+
+
+
+@app.route('/api/v1/gallery', methods=['GET'])
+def gallery():
+   try:
+       container_client = blob_service_client.get_container_client(CONTAINER_NAME)
+       blobs = container_client.list_blobs()
+       return jsonify({
+        "ok": True,
+        "gallery": [f'{container_client.url}/{blob.name}' for blob in blobs],
+   }), 200
+   except Exception as e:
+       return jsonify({
+           'error': str(e)
+       }),500
+
+
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+
+if __name__ == '__main__':
+   app.run()
